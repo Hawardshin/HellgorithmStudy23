bb0df584a63898c3905059994fffea98c5aee1e7
wnddms12345 <wnddms12345@naver.com>
2023-09-04 21:40:42 +0900

    [joushin] 0904

--- FirstWave/joushin/2set/2146.cpp
+++ FirstWave/joushin/2set/2146.cpp
@@ -5,12 +5,23 @@ int N;
 int input[103][103];
 int dy[4] = {0,1,0,-1};
 int dx[4] = {1,0,-1,0};
-int area_val;
+int area_val = 0;
 int min_len = 2147483647;
+// void printall(){
+// 	for(int i=0;i <N;i++){
+// 		for(int j=0; j<N;j++){
+// 			cout << input[i][j];
+// 		}
+// 		cout << "\n";
+// 	}
+// 	cout << "-------------------\n";
+// }
+
 void init(int i, int j){
 	queue<pair<int,int> > Q;
 	Q.push({i,j});//y,x
 	area_val--;
+	input[i][j] = area_val;
 	while (!Q.empty()){
 		auto a = Q.front();
 		int my = a.first;
@@ -43,19 +54,27 @@ void find_mini(int i,int j){//y,x
 			int tx = mx + dx[i];
 			if (ty < 0 || tx < 0 || ty >=N ||tx >= N)
 				continue;
-			if (input[ty][tx] == start_id)//같은 섬이면
+			if (start_id < 0 && input[ty][tx] == start_id)//같은 섬이면
 				continue;
-			else if (input[ty][tx] < 0)//다른 섬이면
+			if (input[ty][tx] < 0)//다른 섬이면
 			{
-				// cout << "다음으로 도착할 곳" << input[ty][tx] << "\n";
-				if (min_len >  input[my][mx]){
+				if (min_len >  input[my][mx])
 					min_len = input[my][mx];
-				}
 				continue;
 			}
-			if (input[ty][tx] == 0){//아직 방문하지 않은 곳이라면 또는 이미 방문했는데 이전에 방문했던 거리보다 더 짧아진 경우
-				// cout << "ty :"<< ty << "tx:" <<tx << "\n";
-				input[ty][tx] = input[my][mx] + 1;
+			if (input[ty][tx] == 0){//아직 방문하지 않은 곳이라면 
+				if (input[my][mx] > 0)
+					input[ty][tx] = input[my][mx] + 1;
+				else
+					input[ty][tx] = 1;
+				Q.push({ty,tx});
+			}
+			else if (input[ty][tx] > 0 && input[ty][tx] > input[my][mx] + 1)
+			{//또는 이미 방문했는데 이전에 방문했던 거리보다 더 짧아진 경우
+				if (input[my][mx] > 0)
+					input[ty][tx] = input[my][mx] + 1;
+				else
+					input[ty][tx] = 1;
 				Q.push({ty,tx});
 			}
 		}
@@ -80,8 +99,9 @@ int main(){
 	}
 	for(int i=0;i<N;i++){//min value find
 		for(int j=0;j<N;j++){
-			if (input[i][j] < 0)
+			if (input[i][j] < 0){
 				find_mini(i,j);
+			}
 		}
 	}
 	cout << min_len << "\n";
