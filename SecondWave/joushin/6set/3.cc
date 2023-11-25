01c86662c0547df07437aa58bbf83d4d6eb0704a
Hawardshin <wnddms12345@naver.com>
2023-11-25 22:13:32 +0900

    FEAT::::::

--- SecondWave/joushin/6set/3.cc
+++ SecondWave/joushin/6set/3.cc
@@ -1,53 +1,41 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 using namespace std;
 int N,K;
-pair<int,int> walk[101];//time, money
-pair<int,int> bike[101];
-int DP_TIME[101];
-int DP_MONEY[101];
-int MINUS_TIME[101];
-int MINUS_MONEY[101];
+pair<int,int> trip[101][2];
+int dp[100001][101];
+#define TIME first 
+#define MONEY second 
 void input(){
-	// ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 	cin >> N >> K;
 	for(int i=0;i<N;i++){
 		int a,b,c,d;
 		cin >> a >>b>>c >>d;
-		walk[i] = {a,b};
-		bike[i] = {c,d};
-		if (i !=0){
-			if (a > c){
-				DP_TIME[i] = DP_TIME[i-1]+ a;
-				DP_MONEY[i] = DP_MONEY[i-1] + b;
-				MINUS_TIME[i] = abs(a-c);
-				MINUS_MONEY[i] = abs(b-d);
-			}else{
-				DP_TIME[i] = DP_TIME[i-1]+ c;
-				DP_MONEY[i] = DP_MONEY[i-1]+d;
-				MINUS_TIME[i] = abs(a-c);
-				MINUS_MONEY[i] = abs(b-d);
-			}
-		}
-		else{
-			if (a > c){
-				DP_TIME[i] =  a;
-				DP_MONEY[i] = b;
-				MINUS_TIME[i] = abs(a-c);
-				MINUS_MONEY[i] = abs(b-d);
-			}else{
-				DP_TIME[i] =  c;
-				DP_MONEY[i] = d;
-				MINUS_TIME[i] = abs(a-c);
-				MINUS_MONEY[i] = abs(b-d);
-			}
-		}
-		cout << "|DP_TIME:" << DP_TIME[i] << " | DP_MONEY:" <<DP_MONEY[i] <<"|\n";
+		trip[i][0] = {a,b};
+		trip[i][1] = {c,d};
 	}
+	memset(dp,-1,sizeof(dp));
+}
+
+int recur(int time, int pos){
+	if (pos >= N) return 0;
+	if (dp[time][pos] != -1) return dp[time][pos];
+	dp[time][pos] = -1000000;
+	if (time + trip[pos][0].TIME <= K)
+		dp[time][pos] = max(dp[time][pos], recur(time+trip[pos][0].TIME,pos+1) + trip[pos][0].MONEY);
+	if (time + trip[pos][1].TIME <= K)
+		dp[time][pos] = max(dp[time][pos],recur(time+trip[pos][1].TIME, pos+1) + trip[pos][1].MONEY);
+	return dp[time][pos];
+}
+
+void solve(){
+	cout << recur(0,0) << "\n";
 }
 
 int main(){
 	input();
-	
+	solve();
 }
