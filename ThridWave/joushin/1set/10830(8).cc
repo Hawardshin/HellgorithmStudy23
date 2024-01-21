fe1fdaf1856b2cd425759cd48b634a85ba609582
Hawardshin <wnddms12345@naver.com>
2024-01-22 02:36:50 +0900

    joushin 0112 앞으로 빨리 하겠습니다..

--- ThridWave/joushin/1set/10830(8).cc
+++ ThridWave/joushin/1set/10830(8).cc
@@ -9,3 +9,44 @@ C*A+D*C C*B+D*D
 A*A+B*C B(A+D)
 C(A+D)  D*D+C*B
 */
+
+#include<iostream>
+using namespace std;
+
+long long n, b;
+long long a[5][5];
+long long tmp[5][5];
+long long ans[5][5];
+
+// 행렬 곱셈
+void mulMat(auto &x, auto &y) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      tmp[i][j] = 0; 
+      for (int k = 0; k < n; k++) tmp[i][j] += (x[i][k] * y[k][j]);
+			tmp[i][j] %= 1000;
+    }
+  }
+  for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)  x[i][j] = tmp[i][j];
+  }
+}
+
+int main() {
+  cin >> n >> b;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) cin >> a[i][j];
+    ans[i][i] = 1;
+  }
+
+  while (b > 0) {
+    if (b % 2 == 1) 
+      mulMat(ans, a);
+	  mulMat(a, a);
+	  b /= 2;
+	}
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) cout << ans[i][j] << " "; 
+    cout << "\n";
+  }
+}
--- ThridWave/joushin/1set/2143(1-2).cc
+++ ThridWave/joushin/1set/2143(1-2).cc
@@ -1,1 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int a[1000],b[1000];
+vector<int> as,bs;
+int T,n,m;
+int main(){
+	cin >> T >> n;
+	for(int i=0;i<n;i++)cin >> a[i];
+	for(int i=0;i<n;i++){
+		int sum = a[i];
+		as.push_back(sum);
+		for(int j=i+1;j<n;j++){
+			sum+=a[j];
+			as.push_back(sum);
+		}
+	}
+	cin >> m;
+	for(int i=0;i<m;i++) cin >> b[i];
+	for(int i=0;i<m;i++){
+		int sum = b[i];
+		bs.push_back(sum);
+		for(int j=i+1;j<m;j++){
+			sum+=b[j];
+			bs.push_back(sum);
+		}
+	}
+
+	sort(as.begin(),as.end());
+
+	long long ans = 0;
+	for(auto it : bs){
+		int diff = T - it;
+		int ub = upper_bound(as.begin(),as.end(),diff) - as.begin();//초과 숫자.
+		int lb = lower_bound(as.begin(),as.end(),diff) - as.begin();//같거나 큰 숫자.
+		ans += (ub - lb);
+	}
+	cout <<  ans <<"\n";
+}
